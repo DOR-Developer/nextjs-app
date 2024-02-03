2c7279eabdb872db2f24ec5d7d46bebfb78f0bf7
DOR-Developer <dor.webdeveloper@gmail.com>
2024-02-03 03:56:53 +0100

    Profile links + component sections + cv section + footer credits + layout tweaks. Added a fancy scroll too!

--- test-page/index.js
+++ test-page/index.js
@@ -1,7 +1,22 @@
-Array.from(document.getElementsByClassName("no-route")).forEach(route => {
-  route.addEventListener("click", (ev) => {
-    // TODO : Change main content.
-  });
+Array.from(document.getElementsByTagName("a")).forEach(route => {
+  const href = route.getAttribute("href");
+  if (href && href.startsWith("#")) {
+    route.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      let id = href.replace("#", "");
+      if (id.length === 0) {
+        window.scrollTo({
+          top: 0, 
+          behavior: "smooth", 
+        });
+      } else {
+        console.log(id);
+        document.getElementById(id).scrollIntoView({
+          behavior: "smooth", 
+        });
+      }
+    });
+  }
 });
 
 document.getElementById("go-to-top").addEventListener("click", () => {
@@ -11,42 +26,45 @@ document.getElementById("go-to-top").addEventListener("click", () => {
   });
 });
 
+document.body.classList.add(localStorage.getItem("fav-theme") ?? "dark");
+
 document.getElementById("change-theme").addEventListener("click", () => {
   const body = document.body;
   const darkTheme = body.classList.contains("dark");
   if (darkTheme) {
     body.classList.remove("dark");
     body.classList.add("light");
+    localStorage.setItem("fav-theme", "light");
   } else {
     body.classList.remove("light");
     body.classList.add("dark");
+    localStorage.setItem("fav-theme", "dark");
   }
 });
 
 setWebLanguage();
 
-const languageButtons = Array.from(document.getElementsByClassName("change-language"));
-
-languageButtons.forEach(button => {
+Array.from(document.getElementsByClassName("change-language")).forEach(button => {
   try {
     button.addEventListener("click", (ev) => {
-      languageButtons.forEach(btn => {
-        btn.classList.remove("selected");
-      });
-      document.body.classList.remove("es");
-      document.body.classList.remove("en");
-  
       const id = ev.target.id;
-  
       const language = id === "language-es" 
         ? "es" : id === "language-en" 
         ? "en" : "en";
   
+      if (document.body.classList.contains(language)) {
+        return;
+      }
+
+      document.body.classList.remove("es");
+      document.body.classList.remove("en");
       document.body.classList.add(language);
-      button.classList.add("selected");
+
       setWebLanguage();
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 function setWebLanguage() {
@@ -54,14 +72,40 @@ function setWebLanguage() {
     const spanish = document.body.classList.contains("es");
     const english = document.body.classList.contains("en");
 
-    const language = spanish ? "es" : english ? "en" : "en";
-    
+    const language = spanish ? "es" : english ? "en" : (localStorage.getItem("fav-language") ?? "en");
+
+    Array.from(document.getElementsByClassName("change-language")).forEach(btn => {
+      btn.classList.remove("selected");
+    });
+    document.getElementById("language-" + language).classList.add("selected");
+
+    localStorage.setItem("fav-language", language);
+
     document.body.classList.add(language);
 
     document.getElementById("about-text").innerHTML = translations[language]["about"];
     document.getElementById("title-text").innerHTML = translations[language]["title"];
-    const routes = Array.from(document.getElementsByTagName("a"));
+    document.getElementById("components-text").innerHTML = translations[language]["components"]["info"];
+    document.getElementById("gallery-component-text").innerHTML = translations[language]["components"]["gallery"];
+    document.getElementById("table-component-text").innerHTML = translations[language]["components"]["table"];
+    document.getElementById("kanban-component-text").innerHTML = translations[language]["components"]["kanban"];
+    
+    document.getElementById("full-cv-text").innerHTML = translations[language]["cv"]["full"];
+    document.getElementById("simplified-cv-text").innerHTML = translations[language]["cv"]["simple"];
+    try {
+      document.getElementById("cv-preview-text").innerHTML = translations[language]["cv"]["preview"];
+      document.getElementById("cv-download-text").innerHTML = translations[language]["cv"]["download"];  
+    } catch (error) {
+      // To prevent browsers that load the iframe from crashing the app.
+    }
 
+    document.getElementById("credits-images-by").innerHTML = translations[language]["credits"]["images"];
+    document.getElementById("credits-icons-by").innerHTML = translations[language]["credits"]["icons"];
+    
+    document.title = translations[language]["html-title"];
+    document.getElementById("cv-preview").setAttribute("src", `./assets/pdf/cv_daniel_otero_rivera_front_dev_${language}.pdf`);
+
+    const routes = Array.from(document.getElementsByTagName("a"));
     routes.forEach(route => {
       try {
         const href = route.getAttribute("href");
@@ -90,12 +134,14 @@ function setWebLanguage() {
         }
       } catch (error) {}
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 setTimeout(() => {
   document.getElementById("profile-pic-loader").classList.remove("loading-border");
-}, 1200);
+}, 750);
 
 let imageSourceCount = 0;
 document.getElementById("profile-pic").addEventListener("click", (ev) => {
@@ -105,9 +151,9 @@ document.getElementById("profile-pic").addEventListener("click", (ev) => {
     document.getElementById("profile-pic-loader").classList.add("loading-border");
     setTimeout(() => {
       document.getElementById("profile-pic-loader").classList.remove("loading-border");
-    }, 1200);
+    }, 750);
 
-    ev.target.setAttribute("src", "https://picsum.photos/" + (800 + imageSourceCount));
+    ev.target.setAttribute("src", "https://picsum.photos/" + (500 + imageSourceCount));
     console.log(ev.target);
   } catch (error) {}
 });
